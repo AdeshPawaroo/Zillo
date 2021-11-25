89d18e92f259c2e5616e1beadabc3448b2b542fd
adesh <adeshpawaroo1@gmail.com>
2021-11-24 20:54:07 -0500

    finish sessions/application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
-end
+    
+    protect_from_forgery with: :exception
+    
+    helper_method :current_user, :logged_in?
+    
+    def current_user 
+      return nil unless session[:session_token]
+      @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+  
+    def ensure_logged_in 
+      render "api/users/show.json.jbuilder" unless logged_in?
+    end  
+  
+    def logged_in? 
+        !!current_user
+    end
+  
+    def login(user)
+      user.reset_session_token!
+      @current_user = user
+      session[:session_token] = @current_user.reset_session_token!
+    end
+  
+    def logout
+        @current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+  end
