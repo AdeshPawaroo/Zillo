3028c709ca052ecc6244e21c2ee671b1fae004c8
adesh <adeshpawaroo1@gmail.com>
2021-11-25 01:44:36 -0500

    update entry file / debbuging routing error

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import { Signup } from './temp/signup';
+
 const App = () => {
     return (
         <div>
-            
+            <Route path="/signup" component={Signup} />
         </div>
     )
 }
--- frontend/zillo.jsx
+++ frontend/zillo.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React from 'react'
 import ReactDOM from "react-dom";
-import configureStore from "./store/store"
+import configureStore from "./store/store";
 import Root from "./components/root";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
-  const root = document.getElementById("root");
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  
+  if (window.currentUser) {
+    const preloadedState = {
+      session: {
+        currentUser: window.currentUser.id
+      },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 
   window.getState = store.getState;
 });
+
