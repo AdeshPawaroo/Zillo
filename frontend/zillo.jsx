111622721f96e1cf253976573eebd23b9e0a868d
adesh <adeshpawaroo1@gmail.com>
2021-11-25 00:45:15 -0500

    finish app & root component

--- frontend/zillo.jsx
+++ frontend/zillo.jsx
@@ -1,33 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import { login, logout, signup } from "./util/session_api_util";
 import configureStore from "./store/store"
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Zillo</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 
-  let store;
-  
-  if (window.currentUser) {
-    const preloadedState = {
-      session: {
-        currentUser: window.currentUser.id
-      },
-      entities: {
-        users: { [window.currentUser.id]: window.currentUser }
-      }
-    };
-    store = configureStore(preloadedState);
-    delete window.currentUser;
-  } else {
-    store = configureStore();
-  }
-
-  window.login = login
-  window.logout = logout
-  window.signup = signup
-
-  window.getState = store.getState
+  window.getState = store.getState;
 });
