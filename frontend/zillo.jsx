5785a65cf936b6c3a21c771f2b3356786cce06dc
adesh <adeshpawaroo1@gmail.com>
2021-11-24 23:32:18 -0500

    finish reducers / store

--- frontend/zillo.jsx
+++ frontend/zillo.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { login, logout, signup } from "./util/session_api_util";
+import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Zillo</h1>, root);
 
+  let store;
+  
+  if (window.currentUser) {
+    const preloadedState = {
+      session: {
+        currentUser: window.currentUser.id
+      },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   window.login = login
   window.logout = logout
   window.signup = signup
+
+  window.getState = store.getState
 });
