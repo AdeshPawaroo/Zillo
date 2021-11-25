c7cb430b2f479bb4a4d79221b7595157053e4e45
adesh <adeshpawaroo1@gmail.com>
2021-11-24 21:48:01 -0500

    finsh & testing backend auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, length: { minimum: 6, allow_nil: true }
     validates :session_token, presence: true, uniqueness: true
-    validates :password_digest, presence: true, uniqueness: true
+    validates :password_digest, presence: true
 
     attr_reader :password
 
