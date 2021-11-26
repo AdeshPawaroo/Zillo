70e4ed8f26475e59549a89d8ec3866b32e29a46d
adesh <adeshpawaroo1@gmail.com>
2021-11-26 18:39:36 -0500

    refactored to loing/signup via modal

--- frontend/components/nav_bar/navbar.jsx
+++ frontend/components/nav_bar/navbar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { openModal } from "../../actions/modal_actions";
+import { logout } from "../../actions/session_actions";
 
 export const Navbar = () => {
+
+    const dispatch = useDispatch();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        dispatch(openModal("login"));
+    }
+
     return (
         <div>
-            <h2>here</h2>
+            <button onClick={handleClick}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -5,7 +5,7 @@ class SessionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      email: '',
       password: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -39,17 +39,17 @@ class SessionForm extends React.Component {
     return (
       <div className="login-form-container">
         <form onSubmit={this.handleSubmit} className="login-form-box">
-          Welcome to BenchBnB!
+          Welcome to Zillo!
           <br/>
           Please {this.props.formType} or {this.props.otherForm}
           <div onClick={this.props.closeModal} className="close-x">X</div>
-          {this.renderErrors()}
+          {/* {this.renderErrors()} */}
           <div className="login-form">
             <br/>
-            <label>Username:
+            <label>email:
               <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
+                value={this.state.email}
+                onChange={this.update('email')}
                 className="login-input"
               />
             </label>
