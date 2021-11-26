0bdcc05b7662288209d0124c851016ae40a8b0d2
adesh <adeshpawaroo1@gmail.com>
2021-11-26 04:13:03 -0500

    refactored login/signup into modals

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,16 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import { Signup } from './session_form/signup';
 import { Login } from './session_form/login';
 import { Splash } from './splash/splash';
-import { Modal } from './modal/modal'
+import Modal from './modal/modal';
+import { Navbar } from './nav_bar/navbar';
 
 const App = () => {
     return (
         <div>
-
+            <Modal />
+            <Navbar />
             <Routes>
-                <Route path="/" element={ <Splash /> }  />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Splash />}  />
+                
+                {/* <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} /> */}
             </Routes>
         </div>
     )
