1cbf1a765666e1f6ad292d8d81b6bb9216141e4c
adesh <adeshpawaroo1@gmail.com>
2021-11-27 18:56:37 -0500

    implement css reset / splash img

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,7 +4,7 @@ export const Splash = () => {
 
     return (
         <div className="splash-container">
-            <h2>Hello this will be the Splash page</h2>
+            <div className="img-container"></div>
         </div>
     ) 
 }
