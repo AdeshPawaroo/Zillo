7234f730dfeeb338dc844d26a9c60b415705f2fb
adesh <adeshpawaroo1@gmail.com>
2021-11-27 19:49:25 -0500

    implement panels/pictures, working on format

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,6 +5,29 @@ export const Splash = () => {
     return (
         <div className="splash-container">
             <div className="img-container"></div>
+
+            <br/ >  <br/ >  <br/ >  <br/ >
+
+            <div className="slogan-container">
+                <p className="slogan">Whether you're buying, selling or renting, we can help you move forward.</p>
+            </div>
+
+            <br/ >  <br/ >  
+
+            <div className="panel-container">
+                <div className="buy-panel">
+                    <span className="buy-img"></span>
+                </div>
+                <div className="sell-panel">
+                    <span className="sell-img"></span>
+                </div>
+                <div className="rent-panel">
+                    <span className="rent-img"></span>
+                </div>
+            </div>
+
+            <br/ >  <br/ >  <br/ >  <br/ >
+
         </div>
     ) 
 }
