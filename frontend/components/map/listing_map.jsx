a0f4aa0c920152d21b2bc1b12b74c25bbf6dd5f9
adesh <adeshpawaroo1@gmail.com>
2021-12-05 03:02:07 -0500

    finish adding markers to the map

--- frontend/components/map/listing_map.jsx
+++ frontend/components/map/listing_map.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import { GoogleMap, Marker } from "@react-google-maps/api"
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GoogleMap, Marker } from "@react-google-maps/api";
+import { fetchListings } from "../../actions/listing_actions";
 
 export const ListingsMap = (props) => {
+
+    const dispatch = useDispatch();
+    const _listings = useSelector(state => state.entities.listings);
+    const listings = Object.values(_listings);
 
     const mapOptions = {
         height: "760px",
@@ -15,6 +21,18 @@ export const ListingsMap = (props) => {
 
     const locations = [];
 
+    // map through listings adding the correct locations for existing listings
+    listings.map((listing, i) => {
+        const listingObj = {
+            id: i + 1,
+            location: {
+                lat: listing.lat,
+                lng: listing.lng
+            }
+        }
+
+        locations.push(listingObj);
+    });
 
     return (
         <GoogleMap
@@ -22,6 +40,16 @@ export const ListingsMap = (props) => {
             zoom={11}
             center={center}
         >
+            {
+                locations.map(item => {
+                    return (
+                        <Marker 
+                            id={item.id}
+                            position={item.location}
+                        />
+                    )
+                })
+            }
         </GoogleMap>
     )
 }
