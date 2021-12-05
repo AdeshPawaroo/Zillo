c210e2eb71d4fad2ba56925339648e25ab56859c
adesh <adeshpawaroo1@gmail.com>
2021-12-04 21:29:09 -0500

    finish skeleton for buy page nav bar

--- frontend/components/buy_page/buy.jsx
+++ frontend/components/buy_page/buy.jsx
@@ -4,11 +4,14 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { ListingsMap } from "../map/listing_map";
 import { ListingsIndex } from "../listing/listings_index";
+import { BuyPageNav } from "./buy_page_nav";
 
 export const BuyPage = (props) => {
 
     return (
         <div className="buy-page-container">
+            <BuyPageNav />
+            
             <div className="buy-page-left">
                 <ListingsMap />
             </div>
