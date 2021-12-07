467c12393d1227e58bde2ae3c68472de3a7128d3
adesh <adeshpawaroo1@gmail.com>
2021-12-07 00:20:25 -0500

    implement basic dropdown

--- frontend/components/nav_bar/navbar_dropdown.jsx
+++ frontend/components/nav_bar/navbar_dropdown.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../actions/session_actions";
 
 export const NavbarDropdown = (props) => {
+
+    const dispatch = useDispatch();
+
+    const handleHouses = (e) => {
+        e.preventDefault();
+    }
+
+    const handleSaved = (e) => {
+        e.preventDefault();
+    }
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+    }
 
     return (
         <div className="dropdown-container">
             <select className="nav-dropdown" name="nav_dropdown">
                 <option>Your houses for sale</option>
                 <option>Your saved houses</option>
-                <option>Log Out</option>
+                <option onClick={handleLogout}>Log Out</option>
             </select>
         </div>
     )
