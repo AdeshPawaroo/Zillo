1cbae0411df520c1197711a74c1cdfa18855abfa
adesh <adeshpawaroo1@gmail.com>
2021-12-06 19:15:28 -0500

    implement nav dropdown / some styling

--- frontend/components/nav_bar/navbar_dropdown.jsx
+++ frontend/components/nav_bar/navbar_dropdown.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-export const NavbardDropdown = (props) => {
+export const NavbarDropdown = (props) => {
 
     return (
-        
+        <div className="dropdown-container">
+            <select className="nav-dropdown" name="nav_dropdown">
+                <option>Your houses for sale</option>
+                <option>Your saved houses</option>
+                <option>Log Out</option>
+            </select>
+        </div>
     )
 }
