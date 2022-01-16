23951a91d96ce87b454f916746b32d8dc316e30a
adesh <adeshpawaroo1@gmail.com>
2022-01-15 19:47:07 -0500

    style new page1 of sell form

--- frontend/components/sell_page/sell_greeting.jsx
+++ frontend/components/sell_page/sell_greeting.jsx
@@ -13,8 +13,25 @@ export const SellGreeting = () => {
                 </p>
             </div>
             <div className="sell-greeting-photo"></div>
-            <div>
-                
+            <div className="sell-info-container">
+                <div className="sell-info-contents">
+                    <h1>
+                        Sell traditionally with Zillo
+                    </h1>
+                    <br />
+                    <p>
+                        When you work with Zillo, you'll get selling support at every step,
+                        <br />
+                        from prepping and listing your home to marketing that gets buyers in the door.
+                    </p>
+                    <br />
+                    <br />
+                    <button className="sell-info-button">Post a listing</button>
+                </div>
+                {/* <div className="sell-info-picture"></div> */}
+                <div>
+                    <img src={window.sell_info_pic} />    
+                </div>
             </div>
         </div>
     )
