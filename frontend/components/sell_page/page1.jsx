8b1e60ffe9ba75e55851818079de4db1884d595b
adesh <adeshpawaroo1@gmail.com>
2022-01-16 16:24:15 -0500

    add bottom half to sell page1

--- frontend/components/sell_page/page1.jsx
+++ frontend/components/sell_page/page1.jsx
@@ -11,13 +11,16 @@ export const Page1 = () => {
             <br />
             <div className="page1-info">
                 <div className="page1-panel">
+                    <img src={window.page1img1} />
                     On Zillo, you can post a listing for free!
                 </div>
                 <div className="page1-panel">
+                <img src={window.page1img2} />
                     Your home will be listed on Zillo and Trulia, reaching
                     the largest audience of home shoppers on the Web.
                 </div>
                 <div className="page1-panel">
+                <img src={window.page1img3} />
                     Home shoppers receive instant emails about new listings.
                 </div>  
             </div>
