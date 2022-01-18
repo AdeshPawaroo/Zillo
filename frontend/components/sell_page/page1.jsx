8fbc7dd3b675b7dc55b4e43a290239172d28d562
adesh <adeshpawaroo1@gmail.com>
2022-01-17 23:40:29 -0500

    implement state for page1

--- frontend/components/sell_page/page1.jsx
+++ frontend/components/sell_page/page1.jsx
@@ -1,10 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Page1 = () => {
+    const [status, setStatus] = useState({
+        status: ""
+    });
+
+    const [style, setStyle] = useState({
+        style: ""
+    });
+
+    const [realator, setRealator] = useState({
+        realator: ""
+    });
+
+    const [values, setValues] = useState({
+        step: 1,
+        price: "",
+        address: "",
+        beds: "",
+        baths: "",
+        zipcode: "",
+        sqft: "",
+        description: "",
+        lat: "",
+        lng: "",
+    });
+
+    const nextStep = () => {
+        const { step } = this.state;
+        setValues({
+            step: step + 1
+        });
+    };
+
+    const prevStep = () => {
+        const { step } = this.state;
+        setValues({
+            step: step - 1
+        });
+    };
+
+    const handleChange = input => e => {
+        setValues({
+            [input]: e.target.value
+        });
+    };
+
+    const handleRealator = (e) => {
+        setRealator({
+            realator: e.target.value
+        });
+    };
+
+    const handleStyle = (e) => {
+        setStyle({
+            style: e.target.value
+        });
+    };
+
+    const handleStatus = (e) => {
+        setStatus({
+            status: e.target.value
+        });
+    };
+    console.log(values.price, "asdasd");
     return(
         <div className="page1-container">
             <div className="page1-contents">
-
+                <h1>Post a Listing by Owner</h1>
+                <div className="page1-form">
+                    <label>Price (Please include a "$" and ","s:</label>
+                    <input type="textarea" 
+                        placeholder={values.price}
+                        onChange={handleChange("price")}
+                        defaultValue={values.price}
+                    />
+                    <label>Status</label>
+                    <select name="status" id="select1" onChange={handleStatus}>
+                        <option value="for sale">For Sale</option>
+                        <option value="for rent">For Rent</option>
+                    </select>
+                </div>
             </div>
             <br />
             <h1>Why post on Zillo?</h1>
