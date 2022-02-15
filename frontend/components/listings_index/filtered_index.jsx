806459c42c65970b7aed2a521c5a4b762cfdefb4
adesh <adeshpawaroo1@gmail.com>
2022-02-14 23:01:07 -0500

    debugging

--- frontend/components/listings_index/filtered_index.jsx
+++ frontend/components/listings_index/filtered_index.jsx
@@ -11,7 +11,13 @@ export const FilteredIndex = (props) => {
     const optionKeys = Object.keys(props.options);
     const allListings = Object.values(useSelector(state => state.entities.listings));
     const allListingsIds = Object.keys(useSelector(state => state.entities.listings));
-    const currentLocationHash = window.location.hash;
+    
+    let valueCount = 0;
+    optionValues.map(value => {
+        if (value !== '') {
+            valueCount++;
+        }
+    });
 
     const [listings, setListings] = useState({
         listings: [],
@@ -54,39 +60,18 @@ export const FilteredIndex = (props) => {
                 listingsIds: temp2
             });
         }else {
-            let temp1 = listings.listings;
-            let temp2 = listings.listingsIds;
-            let flag = false;
-            for (let i = 0; i < temp1.length; i++) {
-                let listing = temp1[i];
-                let id = temp2[i];
-                for (let j = 0; j < optionValues.length; j++) {
-                    let value = optionValues[j];
-                    let key = optionKeys[j]; 
-                    if ((key === "zipcode" || key === "baths" || key === "beds") && listing[key] !== parseInt(value) && value !== '') {
-                        console.log("in the if");
-                        temp1.splice(i, 1);
-                        temp2.splice(i, 1);
-                    } else {
-                        console.log("in the else");
-                    }                                                                                                                   
-                }
-            }
-            setListings({
-                listings: temp1,
-                listingsIds:temp2
-            });
+            
         }
     }, [props.options]);
 
     console.log(listings.listings, "LISTINGS");
+    // console.log(valueCount, "count");
 
     return (
         <div className="buy-page-contents">
             <div className="buy-page-left">
                 <BuyMap />
-            </div>
-            <div className="buy-page-right">
+            </div>            <div className="buy-page-right">
                 here
             </div>
         </div>
@@ -111,3 +96,90 @@ export const FilteredIndex = (props) => {
     //         }
     //     }
     // }
+
+    // useEffect(() => {
+        //     if (listings.listings.length === 0) {
+        //         let temp1 = [];
+        //         let temp2 = [];
+        //         for (let i = 0; i < allListings.length; i++) {
+        //             let listing = allListings[i];
+        //             let id = allListingsIds[i];
+        //             for (let j = 0; j < optionValues.length; j++) {
+        //                 let value = optionValues[j];
+        //                 let key = optionKeys[j];
+        //                 if (value === '') {
+        //                     continue;
+        //                 }else if (key === "zipcode" || key === "beds" || key === "baths") {
+        //                     if (listing[key] === parseInt(value)) {
+        //                         temp1.push(listing);
+        //                         temp2.push(id);
+        //                     }
+        //                 }else if (listing[key] === value) {
+        //                     temp1.push(listing);
+        //                     temp2.push(id);
+        //                 }
+        //             }
+        //         }
+        //         setListings({
+        //             listings: temp1,
+        //             listingsIds: temp2
+        //         });
+        //     }else {
+        //         let temp1 = listings.listings;
+        //         let temp2 = listings.listingsIds;
+        //         for (let i = 0; i < temp1.length; i++) {
+        //             let listing = temp1[i];
+        //             let id = temp2[i];
+        //             for (let j = 0; j < optionValues.length; j++) {
+        //                 let value = optionValues[j];
+        //                 let key = optionKeys[j]; 
+        //                 if ((key === "zipcode" || key === "baths" || key === "beds") && listing[key] !== parseInt(value) && value !== '') {
+        //                     console.log(temp1, "CURRENT");
+        //                     temp1.splice(i, 1);
+        //                     temp2.splice(i, 1);
+        //                 } else {
+        //                     if (listing[key] !== value && value !== '') {
+        //                         // temp1.splice(i, 1);
+        //                         // temp2.splice(i, 1);
+        //                     }
+        //                 }                                                                                                                   
+        //             }
+        //         }
+        //         setListings({
+        //             listings: temp1,
+        //             listingsIds:temp2
+        //         });
+        //     }
+        // }, [props.options]);
+
+
+
+        // let temp1 = [];
+        //     let temp2 = [];
+        //     let flag = false;
+        //     console.log(temp1.length);
+        //     for (let i = 0; i < listings.listings.length; i++) {
+        //         let listing = listings.listings[i];
+        //         let id = listings.listingsIds[i];
+        //         for (let j = 0; j < optionValues.length; j++) {
+        //             let value = optionValues[j];
+        //             let key = optionKeys[j];
+        //             if (value === '') {
+        //                 continue;
+        //             }else if ((key === "zipcode" || key === "baths" || key === "beds") && listing[key] === parseInt(value)) {
+        //                 flag = true;
+        //             }else if ((key === "realator" || key === "style") && listing[key] === value) {
+        //                 flag = true;
+        //             }else {
+        //                 flag = false;
+        //             }
+        //         }
+        //         if (flag === true) {
+        //             temp1.push(listing);
+        //             temp2.push(id);
+        //             setListings({
+        //                 listings: temp1,
+        //                 listingsIds: temp2
+        //             });
+        //         }
+        //     }
