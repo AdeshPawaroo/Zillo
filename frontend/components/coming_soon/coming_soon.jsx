f013d0d86a2f306aac268e34fa5e4fb4cbd29205
AdeshPawaroo <adeshpawaroo1@gmail.com>
2022-03-07 02:36:02 -0500

    create under construction page

---
+++ frontend/components/coming_soon/coming_soon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const ComingSoon = (props) => {
+    return (
+        <div className="coming-soon-container">
+            <div className="coming-soon-photo"></div>
+            <br />
+            <div className="coming-soon-message">
+                Sorry! 
+                <br />
+                This feature is currently under construction and coming soon!    
+            </div>
+        </div>
+    )
+}
