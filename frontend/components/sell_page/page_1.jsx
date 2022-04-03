bd3b93dadbf89953a71189ab561a4e61bc88b8ef
adesh <adeshpawaroo1@gmail.com>
2022-04-03 15:48:16 -0400

    add inputs / styling dropdown

--- frontend/components/sell_page/page_1.jsx
+++ frontend/components/sell_page/page_1.jsx
@@ -16,36 +16,66 @@ export const Page_1 = (props) => {
                         <div className="page-form-r1">
                             <div className="page-form-element">
                                 <h1>ADDRESS*</h1>
-                                <input type="textarea"/>
+                                <input type="textarea"
+                                    placeholder={values.address}
+                                    onChange={handleChange("address")}
+                                    defaultValue={values.address}
+                                />
                             </div>
                             <div className="page-form-element">
                                 <h1>ZIPCODE*</h1>
-                                <input type="textarea"/>
+                                <input type="textarea"
+                                    placeholder={values.zipcode}
+                                    onChange={handleChange("zipcode")}
+                                    defaultValue={values.zipcode}
+                                />
                             </div>
                         </div>
                         <br />
                         <div className="page-form-r2">
                             <div className='single-page-form-element'>
                                 <h1>PRICE*</h1>
-                                <input type="textarea" />
+                                <input type="textarea" 
+                                    placeholder={values.price}
+                                    onChange={handleChange("price")}
+                                    defaultValue={values.price}
+                                />
                             </div>
                         </div>
                         <br />
                         <div className="page-form-r3">
                             <div className="page-form-element">
                                 <h1>STATUS*</h1>
+                                <input type="textarea" />
                             </div>
                             <div className="page-form-element">
                                 <h1>REALATOR*</h1>
+                                <select className="realator-select" name="status" id="select2" onChange={handleChange("realator")}>
+                                    <option value="none" selected disabled hidden>Select an Option</option>
+                                    <option value="Crown Realators">Crown Realators</option>
+                                    <option value="Empire Estates">Empire Estates</option>
+                                    <option value="Royalty Regals">Royalty Regals</option>
+                                    <option value="Sunshine Agency">Sunshine Agency</option>
+                                 </select>
                             </div>
                         </div>
                         <br />
                         <div className="page-form-r4">
                             <div className="page-form-element">
                                 <h1>LATITUDE*</h1>
+                                <input type="textarea" 
+                                    placeholder={values.lat}
+                                    onChange={handleChange("lat")}
+                                    defaultValue={values.lat}
+                                />
                             </div>
                             <div className="page-form-element">
                                 <h1>LONGITUDE*</h1>
+                                <input type="textarea" 
+                                    placeholder={values.lng}
+                                    onChange={handleChange("lng")}
+                                    defaultValue={values.lng}
+                                />
                             </div>
                         </div>
                         <br />
