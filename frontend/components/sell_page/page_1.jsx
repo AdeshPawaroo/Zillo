d634bf8abd19c2466bda6d0e1d1602f124925a6a
adesh <adeshpawaroo1@gmail.com>
2022-04-03 04:36:07 -0400

    finish restyle page1/add fontawesome arrow

--- frontend/components/sell_page/page_1.jsx
+++ frontend/components/sell_page/page_1.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 export const Page_1 = (props) => {
+    library.add(faArrowRight);
     const { values, nextStep, handleChange } = props;
 
     return (
@@ -43,6 +47,14 @@ export const Page_1 = (props) => {
                             <div className="page-form-element">
                                 <h1>LONGITUDE*</h1>
                             </div>
+                        </div>
+                        <br />
+                        <br />
+                        <br />
+                        <div className='page-form-buttons'>
+                            <button onClick={nextStep}>CONTINUE
+                                {" "}<FontAwesomeIcon icon={faArrowRight} onClick={nextStep}/>
+                            </button>
                         </div>
                     </form>
                 </div>
